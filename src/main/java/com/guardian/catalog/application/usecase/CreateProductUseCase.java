0be5d00f25191c0a14f873ff58c293c7b914c809
MiqueiasAmorimm <miqueias202201@hotmail.com>
2026-04-28 00:13:26 -0300

    feat(catalog): implement CreateProductUseCase

--- src/main/java/com/guardian/catalog/application/usecase/CreateProductUseCase.java
+++ src/main/java/com/guardian/catalog/application/usecase/CreateProductUseCase.java
@@ -1,6 +1,8 @@
 package com.guardian.catalog.application.usecase;
 
+import com.guardian.catalog.domain.model.Product;
 import com.guardian.catalog.infrastructure.repository.ProductRepository;
+import com.guardian.catalog.web.dto.CreateProductRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +11,10 @@ public class CreateProductUseCase  {
     private final ProductRepository productRepository;
     public CreateProductUseCase(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+    public Product execute (CreateProductRequest request) {
+        Product product = new Product (request.getName(),
+                request.getDescription(), request.getPrice());
+        return productRepository.save(product);
     }
 }
