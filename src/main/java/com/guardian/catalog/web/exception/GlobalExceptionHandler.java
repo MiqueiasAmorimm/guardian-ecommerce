cacfbba92aadc6bbc6f1ed3fd9fc48b943c5d480
MiqueiasAmorimm <miqueias202201@hotmail.com>
2026-06-11 00:27:09 -0300

    feat(catalog): add GlobalExceptionHandler for IllegalArgumentException

---
+++ src/main/java/com/guardian/catalog/web/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.guardian.catalog.web.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+
+public class GlobalExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+}
