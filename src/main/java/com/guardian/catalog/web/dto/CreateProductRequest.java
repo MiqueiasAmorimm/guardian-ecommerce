dbaec0d600c62c7739f274b239288060dbafbbb2
MiqueiasAmorimm <miqueias202201@hotmail.com>
2026-06-29 02:33:55 -0300

    feat(catalog): add @AllArgsConstructor to CreateProductRequest

--- src/main/java/com/guardian/catalog/web/dto/CreateProductRequest.java
+++ src/main/java/com/guardian/catalog/web/dto/CreateProductRequest.java
@@ -3,10 +3,12 @@ package com.guardian.catalog.web.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.math.BigDecimal;
 
+@AllArgsConstructor
 @Getter
 public class CreateProductRequest {
     @NotBlank
